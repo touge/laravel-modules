fcd5560f64c6826ba4d5cf36cbf39a59040e6663
NickYuan <yuanlong@leoedu.com>
2018-08-11 20:53:21 +0800

    add BaseModel

---
+++ src/Support/BaseModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nick
+ * Date: 2018/8/11
+ * Time: 下午8:50
+ */
+
+namespace Touge\Modules\Support;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends Model
+{
+
+}
